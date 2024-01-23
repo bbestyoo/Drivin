55d592ea9bd4213a9f6d802e0c0c965b6b79261b
Milanpraz <mln.praz12@gmail.com>
2024-01-23 14:21:29 +0545

    agent

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,6 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/bundle";
-import Agents from "./Agents";
 
 export const Home = () => {
   return (
