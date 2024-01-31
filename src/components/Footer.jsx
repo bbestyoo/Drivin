4b277d8067ebea7caf010dec2c531e971c0bf524
Milanpraz <mln.praz12@gmail.com>
2024-01-31 07:02:59 +0545

    hawa

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,10 +47,24 @@ export default function Footer() {
           <div className="flex justify-between p-5 bg-footer pt-20 pl-10 pr-10 text-white">
             <div className="  p-5 rounded-3xl px-10">
               <p className="text-2xl font-bold ">Get In Touch</p>
-              <p className="flex items-center gap-3 my-4 "><FaCar size={30}/><span className="text-3xl font-bold text-yellow-400">Drivin</span></p>
-              <p className="flex items-center gap-3 mb-2"><FaLocationDot/>123 Street, New York, USA</p>
-              <p className="flex items-center gap-3 mb-2"><FaPhone/>+012 345 67890</p>
-              <p className="flex items-center gap-3"><IoMdMail />info@example.com</p>
+              <p className="flex items-center gap-3 my-4 ">
+                <FaCar size={30} />
+                <span className="text-3xl font-bold text-yellow-400">
+                  Drivin
+                </span>
+              </p>
+              <p className="flex items-center gap-3 mb-2">
+                <FaLocationDot />
+                123 Street, New York, USA
+              </p>
+              <p className="flex items-center gap-3 mb-2">
+                <FaPhone />
+                +012 345 67890
+              </p>
+              <p className="flex items-center gap-3">
+                <IoMdMail />
+                info@example.com
+              </p>
             </div>
 
             <div className=" p-5 rounded-3xl px-10 text-white">
@@ -88,7 +102,7 @@ export default function Footer() {
               <ul className="text-left flex flex-col gap-1">
                 <Link>
                   <li className="flex gap-1 items-center">
-                    <MdKeyboardArrowRight size={27}  /> About Us
+                    <MdKeyboardArrowRight size={27} /> About Us
                   </li>
                 </Link>
                 <Link>
@@ -129,29 +143,37 @@ export default function Footer() {
               <div className="my-4">
                 <p className="font-bold text-xl">Follow us</p>
                 <ul className="flex mt-4 gap-1">
-                  <li  className="border border-white p-3 hover:bg-white hover:text-black">< FaTwitter /></li>
-                  <li  className="border border-white p-3 hover:bg-white hover:text-black">< FaFacebookF /></li>
-                  <li  className="border border-white p-3 hover:bg-white hover:text-black">< FaYoutube /></li>
-                  <li  className="border border-white p-3 hover:bg-white hover:text-black">< FaLinkedinIn /></li>
+                  <li className="border border-white p-3 hover:bg-white hover:text-black">
+                    <FaTwitter />
+                  </li>
+                  <li className="border border-white p-3 hover:bg-white hover:text-black">
+                    <FaFacebookF />
+                  </li>
+                  <li className="border border-white p-3 hover:bg-white hover:text-black">
+                    <FaYoutube />
+                  </li>
+                  <li className="border border-white p-3 hover:bg-white hover:text-black">
+                    <FaLinkedinIn />
+                  </li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-        <div className="bg-last flex justify-between px-16 py-7">
-          <div className="flex gap-1">
+        <div className="bg-last  grid    w-full  grid-cols-2 px-16 py-7">
+          <div className=" gap-1">
             <span className="text-white">© </span>
             <span className="text-yellow-400">Your Site Name,</span>
             <span className="text-white">All Right Reserved.</span>
           </div>
-          <div>
-            <span className="flex justify-end gap-1">
-              <p className="text-white">Designed By </p>
-              <p className="text-yellow-400"> HTML Codex</p>
+          <div className="  text-right">
+            <span className=" gap-1  block">
+              <p className="text-white inline-block">Designed By </p>
+              <p className="text-yellow-400 inline-block"> HTML Codex</p>
             </span>
-            <span className="flex gap-1 ">
-              <p className="text-white">Distributed By: </p>
-              <p className="text-yellow-400"> ThemeWagon</p>
+            <span className="gap-1 ">
+              <p className="text-white inline-block">Distributed By: </p>
+              <p className="text-yellow-400 inline-block"> ThemeWagon</p>
             </span>
           </div>
           {isVisible && (
