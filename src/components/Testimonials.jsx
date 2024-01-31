9daf1ac86b00c98fba92560efa54ef09b96297ff
Milanpraz <mln.praz12@gmail.com>
2024-01-31 07:12:22 +0545

    caroudel

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,6 +3,13 @@ import pp2 from "../../public/pp2.jpg";
 import pp3 from "../../public/pp3.jpg";
 import pp4 from "../../public/pp4.jpg";
 
+// import Swiper JS
+// import Swiper from "swiper";
+// import Swiper styles
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 const mock_data = [
   {
     id: 1,
@@ -43,29 +50,45 @@ const mock_data = [
 
 function Testimonials() {
   return (
-    <div>
-      <section
-        id="for the carousel box"
-        className=" flex flex-col items-center "
+    <>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
       >
-        <div>
-          <img
-            className=" w-36 h-36 object-cover object-top rounded-full"
-            alt="pp"
-            src={pp1}
-          />
-        </div>
-        <div>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quod
-            consequuntur iusto maiores molestiae.
-          </p>
-        </div>
-        <hr />
-        <p>Bibesh Basner</p>
-        <p>Engineer</p>
-      </section>
-    </div>
+        {mock_data.map((slide, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <Card user={slide} />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      {/* <div>
+        <section
+          id="for the carousel box"
+          className=" flex flex-col items-center "
+        >
+          <div>
+            <img
+              className=" w-36 h-36 object-cover object-top rounded-full"
+              alt="pp"
+              src={pp1}
+            />
+          </div>
+          <div>
+            <p className=" text-2xl text-gray-400">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam
+              quod consequuntur iusto maiores molestiae.
+            </p>
+          </div>
+          <hr />
+          <p>Bibesh Basner</p>
+          <p>Engineer</p>
+        </section>
+      </div> */}
+    </>
   );
 }
 
@@ -74,12 +97,19 @@ export default Testimonials;
 export const Card = ({ user }) => {
   return (
     <>
-      <section id="for the carousel box">
+      <section
+        id="for the carousel box "
+        className=" flex flex-col items-center "
+      >
         <div>
-          <img alt="pp" src={user.image} />
+          <img
+            className=" w-36 h-36 object-cover object-top rounded-full"
+            alt="pp"
+            src={user.image}
+          />
         </div>
         <div>
-          <p>{user.character}</p>
+          <p className=" text-2xl text-gray-400">{user.character}</p>
         </div>
         <hr />
         <p>{user.name}</p>
